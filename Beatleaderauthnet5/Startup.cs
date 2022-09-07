e18e65a6466ae841bbcfb6cfcab9e4eb62bfec4d
ethan <ethan@ASUS-TUF>
2022-09-07 12:32:02 +1200

    Fixed Register and Login Buttons

--- Beatleaderauthnet5/Startup.cs
+++ Beatleaderauthnet5/Startup.cs
@@ -45,12 +45,14 @@ namespace Beatleaderauthnet5
             app.UseRouting();
 
             app.UseAuthorization();
+            
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
